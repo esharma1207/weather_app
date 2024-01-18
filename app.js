a7a691bf5926f666f9965b507358203a7e1a236c
Eshaan <es887@cornell.edu>
2024-01-18 02:54:07 -0500

    queried all information from html file to be updated according to data from the API fetch to be updated based on city name that is searched

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 const api_key = "26739281d4846f5ff47eb2b1cee63f79"
-const api_url = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=delhi"
+const api_url = "https://api.openweathermap.org/data/2.5/weather?units=imperial"
 
-async function checkWeather(){
-    const res = await fetch(api_url + `&appid=${api_key}`);
+const searchbar = document.querySelector(".search input");
+const button = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const res = await fetch(api_url + `&q=${city}`  + `&appid=${api_key}`);
     var data = await res.json()
 
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+    document.querySelector(".humid").innerHTML = data.main.humidity + "%";
+    document.querySelector(".windspeed").innerHTML = data.wind.speed + " mph";
+    
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "images/snow.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+
+
+
+
 }
+
+button.addEventListener("click", () => {
+
+    checkWeather(searchbar.value);
+}
+
+)
+
